25205d0abb687a0a2671ce62d1063bc9e93ef731
Fardeen Idrus <fardeenidrus123@gmail.com>
2025-10-19 17:11:33 +0100

    "find_closest_number_to_zero"

--- find_closest_number_to_zero.py
+++ find_closest_number_to_zero.py
@@ -1,1 +1,20 @@
-print('hello world')
+from typing import List
+
+
+class Solution:
+    def findClosestNumber(self, nums: List[int]) -> int:
+        closest = nums[0]
+        for i in nums:
+            if abs(i) < abs(closest):
+                closest = i
+        if closest < 0 and abs(closest) in nums:
+            return abs(closest)
+        else:
+            return closest
+        
+# Time Complexity: O(n²) - we loop O(n) and do an O(n) lookup with "in nums"
+# Space Complexity: O(1) - only using a few variables, no extra data structures
+
+if __name__ == "__main__":
+    solution = Solution()
+    print(solution.findClosestNumber(nums = [-4,-2,1,4,8]))
