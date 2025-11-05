f5852d50846db04f02e7abab3b633ee319c5edad
Fardeen Idrus <fardeenidrus123@gmail.com>
2025-11-05 02:07:22 +0000

    "problem 228"

--- problem228.py
+++ problem228.py
@@ -1,12 +1,15 @@
 from typing import List
 class Solution:
     def summaryRanges(self, nums: List[int])-> List[str]:
+        if not nums:
+            return []
         start = nums[0]
         result = []
         end = nums[0]
         for i in range(1,len(nums)):
             if nums[i] == end +1 :
                 end = nums[i]
+            
             else:
                 if start == end :
                     result.append(str(start))
