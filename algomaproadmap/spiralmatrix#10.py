722ddbb1ee1932f32c6c0ee076748446d3398a22
Fardeen Idrus <fardeenidrus123@gmail.com>
2026-05-28 21:31:00 +0100

    "add comments to #10"

--- algomaproadmap/spiralmatrix#10.py
+++ algomaproadmap/spiralmatrix#10.py
@@ -9,23 +9,34 @@ class Solution:
         right_column = len(matrix[0])-1
         matrix_elements = []
         while top_row <= bottom_row and left_column <= right_column:
+            
+            #Append felements in irst row 
             for i in range(left_column, right_column+1):
                 matrix_elements.append(matrix[top_row][i])
-            
+                
+            #First row visited so we move the row wall down
             top_row +=1
+            
+            # Append elements in the right most column moving down
             for i in range(top_row, bottom_row+1):
                 matrix_elements.append(matrix[i][right_column])
             
+            # Right most column visited, so we move the right column wall to the left
             right_column -=1
             
             
+            # Bottom row may already be visited - only traverse if rows remain
             if top_row <= bottom_row:
                 
+            # Append elements in the bottom row moving left
                 for i in range(right_column, left_column-1, -1):
+                    # Append elements in the left most column moving up
                     matrix_elements.append(matrix[bottom_row][i])
                     
                 bottom_row -=1
             
+            
+            # Left column may already be visited - only travese if column remains 
             if left_column <= right_column:
                     
                 for i in range(bottom_row, top_row-1,-1):
