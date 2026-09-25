1543344514d12613c550db2184f721f82501cb33
Fardeen Idrus <fardeenidrus123@gmail.com>
2026-09-25 22:28:53 +0100

    "add comments"

--- algomaproadmap2026/hashmap_and_sets/two_sum_#6_cleaner_solution.py
+++ algomaproadmap2026/hashmap_and_sets/two_sum_#6_cleaner_solution.py
@@ -2,20 +2,25 @@ from collections import defaultdict
 
 class Solution:
     def twoSum(self, nums: list[int], target: int) -> list[int]:
-        num_dict= {}
-        
+        # Maps a number we've already seen to its index
+        num_dict = {}
+
         for i in range(len(nums)):
+            # The number that would complete the pair with nums[i]
             remainder = target - nums[i]
+
+            # If we've already seen that number, we've found the pair
             if remainder in num_dict:
                 return [num_dict[remainder], i]
-            num_dict[nums[i]] =i 
-            
+
+            # Otherwise, record this number so later numbers can find it
+            num_dict[nums[i]] = i
+
         return False
-    
+
 
 if __name__ == "__main__":
     sol = Solution()
-    print(sol.twoSum([2,7,11,15],9))
-    print(sol.twoSum([3,2,4], 6))
-    print(sol.twoSum([3,3],6 ))
-    
+    print(sol.twoSum([2, 7, 11, 15], 9))
+    print(sol.twoSum([3, 2, 4], 6))
+    print(sol.twoSum([3, 3], 6))
